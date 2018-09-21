0a265906add4c15fc959263a7925ee5ad76f3710
Tao Li <tao.li@fixstream.com>
2018-09-20 18:06:31 -0700

    adding more methods

--- main/java/com/file/ReadFromFile.java
+++ main/java/com/file/ReadFromFile.java
@@ -8,18 +8,21 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class ReadFromFile {
 
     public static void main(String[] args) throws IOException{
 	//List<String> result = readFileNoneBlocking("/usr/share/dict/connectives", "r");
-	List<String> result = readFileBlocking2("/usr/share/dict/connectives");
-	//result.stream().sorted().map(s -> s.toUpperCase()).filter(s -> s.length()==3).forEach(System.out::println);
-	//System.out.println("total: "+result.stream().count());
+	List<String> result = readFileWithScanner("/usr/share/dict/connectives");
+	result.stream().sorted().map(s -> s.toUpperCase()).filter(s -> s.length()==3).forEach(System.out::println);
+	System.out.println("total: "+result.stream().count());
     }
     
    
@@ -54,7 +57,7 @@ public class ReadFromFile {
 	return result;
     }
     
-    // conventional method
+    // conventional BufferedReader method
     private static List<String> readFileBlocking2(String fileName) {
 	List<String> result = new ArrayList<>();
 	File file = new File(fileName);
@@ -70,5 +73,31 @@ public class ReadFromFile {
 	}
 	
 	return result;
+    }
+    
+    // Using Files and Paths
+    private static List<String> readUsingFilesAndPaths(String fileName){
+	List<String> result = null;
+	
+	try(BufferedReader reader = Files.newBufferedReader(Paths.get(fileName))){
+	    result = reader.lines().collect(Collectors.toList());
+	}catch(IOException e){
+	    e.printStackTrace();
+	}
+	return result;
+    }
+    
+    // using scanner
+    private static List<String> readFileWithScanner(String fileName){
+	List<String> result = new ArrayList<>();
+	
+	try(Scanner scanner = new Scanner(new File(fileName))){
+	    while(scanner.hasNextLine())
+		result.add(scanner.nextLine());
+	}catch(IOException e){
+	    e.printStackTrace();
+	}
+	return result;
+	
     }
 }
